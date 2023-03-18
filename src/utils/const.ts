9a3afc642379fbbbbfbb6accfd6aa09ae359198b
Louis <louisandrew3@gmail.com>
2023-03-18 09:09:32 +0100

    feat(const): add personal to const

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,4 +1,4 @@
 export const CLONE_SELECT_VALUE = 'Clone a git repo';
 
 // TODO: Get roots from a config file.
-export const ROOTS = ['dev/ui', 'peepee', 'dev/backend', 'dev/util', 'dev/ui/libs', 'dev/htw/kbe', 'dev/htw/avt'];
+export const ROOTS = ['dev/ui', 'dev/backend', 'dev/utils', 'dev/personal'];
